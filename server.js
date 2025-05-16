4201314f4dfbce61a7675add3cd6dbc5636bfb81
Leandro Coelho <115176955+134ndr0@users.noreply.github.com>
2025-05-15 21:30:47 -0300

    ATLZ_LEANDRO_SRC_15052025_3

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 //importar o obj api
 const api = require('./src/api')
 
-
 //inicializar o servidor
 api.listen(process.env.PORT,()=>{
     console.log('\n API INICIALIZADA!!')
